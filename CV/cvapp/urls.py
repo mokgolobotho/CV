ab5b582161b4362f442abde4d5da91b689356be3
221026802 <221026802@tut4life.ac.za>
2024-10-09 11:55:04 +0200

    finalcommit

--- CV/cvapp/urls.py
+++ CV/cvapp/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path, include
-from .views  import authView, home
+from .views  import authView, home,view_cv
 
 urlpatterns = [
     path("", home, name="home"),
-     path("accounts/", include("django.contrib.auth.urls")),
-     path("signup/", authView, name="authView"),
+    path("accounts/", include("django.contrib.auth.urls")),
+    path("signup/", authView, name="authView"),
+    path("view_cv/<int:cv_id>", view_cv, name="view_cv"),
 ]
--- CV/cvapp/views.py
+++ CV/cvapp/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.decorators import login_required
 from .models import File, Cv
@@ -10,15 +10,20 @@ def home(request):
     if request.method == "POST":
         file = request.FILES['file']
         obj = File.objects.create(file=file)
-        create_db(obj.file.path)
+        cv = create_db(obj.file.path)
+        redirect('view_cv', cv.id_number)
     return render(request, "home.html", {})
+
+def view_cv(request, cv_id):
+    cv = Cv.objects.get(id_number=cv_id)
+    return render(request, "view_cv.html", {'cv': cv})
 
 def create_db(file_path):
     df = pd.read_csv(file_path, delimiter=',',  encoding='ISO-8859-1', on_bad_lines='skip')
     print(df.values)
     list_of_csv = [list(row) for row in df.values]
     for l in list_of_csv:
-        Cv.objects.create(
+        new_cv = Cv.objects.create(
             first_name = l[0],
             last_name = l[1],
             email = l[2],
@@ -30,6 +35,7 @@ def create_db(file_path):
             skills = l[8],
             experience= l[9],
         )
+        return new_cv
 
 def authView(request):
     if request.method == "POST":
